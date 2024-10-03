2badf8bccffa5f6a1439d8f513cb1581d337bbd5
Monica Munoz Sanchez <monmunoz@student.42madrid.com>
2024-10-03 20:49:34 +0200

    primeras pruebas redirecciones

---
+++ redir.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
+
+int	main(void)
+{
+	int		fd[2];
+	pid_t	pid;
+	int		op;
+	int		op2;
+	char	*args[3];
+	char	*argu[3];
+
+	args[0] = "ls";
+	args[1] = "-l";
+	args[2] = NULL;
+	argu[0] = "cat";
+	argu[1] = "-e";
+	argu[2] = NULL;
+	if (pipe(fd) == -1)
+	{
+		perror("Pipe error\n");
+		return (1);
+	}
+	pid = fork();
+	if (pid < 0)
+	{
+		perror("Fork error\n");
+		return (1);
+	}
+	if (pid == 0)
+	{
+		op = open("file1.txt", O_RDWR);
+		if (op < 0)
+			return (1);
+		dup2(op, 0);
+		close(fd[0]);
+		dup2(fd[1], 1);
+		execve("/bin/ls", args, NULL);
+	}
+		op2 = open("file2.txt", O_RDWR | O_CREAT, 0777);
+		if (op2 < 0)
+			return (1);
+		close(fd[1]);
+		dup2(fd[0], 0);
+		dup2(op2, 1);
+		execve("/bin/cat", argu, NULL);
+	return (0);
+}
